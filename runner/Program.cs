e4732a2f994e7c5cf39afb76c62cbdff1f0f6ca5
SakarSilwal <sakarsilwal123@gmail.com>
2024-08-15 19:56:06 +0545

    new lines added

--- runner/Program.cs
+++ runner/Program.cs
@@ -11,6 +11,8 @@ namespace runner
 		static void Main(string[] args)
 		{
             Console.WriteLine("hello world");
-        }
+			Console.WriteLine("hello world");
+			Console.WriteLine("hello world");
+		}
 	}
 }
